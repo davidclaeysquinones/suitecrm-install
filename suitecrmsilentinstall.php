0828405a616e849926b265dbd3ef241a0dc07419
davidclaeysquinones <davidclaeys@outlook.com>
2019-09-09 11:13:12 +0200

    Update suitecrmsilentinstall.php

--- suitecrmsilentinstall.php
+++ suitecrmsilentinstall.php
@@ -24,7 +24,7 @@ function getArguments(){
 function createConfigFile($args){
     $installLoc = $args['install_location'];
     $config = createConfigArray($args);
-    $contents = ''$sugar_config_si = '.var_export($config,1).";\n";
+    $contents = '<?php'."\n".'$sugar_config_si = '.var_export($config,1).";\n";
     file_put_contents($installLoc.'/config_si.php',$contents);
 }
 
